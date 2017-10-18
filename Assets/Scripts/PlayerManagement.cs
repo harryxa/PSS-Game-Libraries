d389663d2076a6929b803ed58c00c109643a1ea8
cjp2-patay <scalywags123@googlemail.com>
2017-10-18 10:39:26 +0100

    Have some Player things!

---
+++ Assets/Scripts/PlayerManagement.cs
@@ -0,0 +1,209 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerManagement : MonoBehaviour {
+
+    public static PlayerManagement Instance;
+
+   public bool player1 = false;
+    public bool player2 = false;
+   public  bool player3 = false;
+    public bool player4 = false;
+    public int player1Lives = 3;
+    public int player2Lives = 3;
+    public int player3Lives = 3;
+    public int player4Lives = 3;
+    public int playerCount = 0;
+
+    public GameObject player1Object;
+    public GameObject player2Object;
+    public GameObject player3Object;
+    public GameObject player4Object;
+    
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Scene scene = SceneManager.GetActiveScene();
+        //Debug.Log(scene);
+        if (scene.name == "Lobby")
+        {
+            if (Input.GetButton("BackToMenu"))
+            {
+                SceneManager.LoadScene(0);
+            }
+
+            if (Input.GetButton("P1Confirm"))
+            {
+                if (player1 == false)
+                {
+                    player1 = true;
+                    Debug.Log(player1);
+                    playerCount++;
+                }
+                
+                
+            }
+            else if(Input.GetButton("P1Cancel"))
+            {
+                if(player1 == true)
+                {
+                    playerCount--;
+                    player1 = false;
+                    
+                    Debug.Log("PLayer 1 cancelled");
+                }
+                
+                
+                
+                
+            }
+
+            if (Input.GetButton("P2Confirm"))
+            {
+                if (!player2)
+                {
+                    player2 = true;
+                    
+                    playerCount++;
+                }
+            }
+            else if (Input.GetButton("P2Cancel"))
+            {
+                if (player2)
+                {
+                    player2 = false;
+                    playerCount--;
+                }
+            }
+
+            if (Input.GetButton("P3Confirm"))
+            {
+                if (!player3)
+                {
+                    player3 = true;
+                    playerCount++;
+                }
+            }
+            else if (Input.GetButton("P3Cancel"))
+            {
+                if (player3)
+                {
+                    player3 = false;
+                    playerCount--;
+                }
+            }
+
+            if (Input.GetButton("P4Confirm"))
+            {
+                if (!player4)
+                {
+                    player4 = true;
+                    playerCount++;
+                }
+            }
+            else if (Input.GetButton("P4Cancel"))
+            {
+                if (player4)
+                {
+                    player4 = false;
+                    playerCount--;
+                }
+            }
+
+        }
+
+        if(Input.GetButton("Tim's Level"))
+        {
+            Scene scene1 = SceneManager.GetActiveScene();
+            if (scene.name == "Lobby")
+            {
+                if (playerCount >= 1)
+                {
+                    SceneManager.LoadScene(2);
+                }
+            }
+        }
+		
+	}
+
+    void Awake()
+    {
+        
+       if (Instance == null)
+       {
+            DontDestroyOnLoad(this.gameObject);
+            Instance = this;
+            //Debug.Log(player1);
+        }
+       else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        //Debug.Log(player1);
+       // Debug.Log(player2);
+       // Debug.Log(player3);
+       // Debug.Log(player4);
+    }
+
+    void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        Scene scene1 = SceneManager.GetActiveScene();
+        if (scene.name != "Lobby" && scene.name != "Callan")
+        {
+            Debug.Log(player1);
+            if (player1)
+            {
+           // if (player1Lives != 0)
+           // {
+                Instantiate(player1Object);
+                player1Object.transform.position = new Vector3(-5f, 2.0f, 0.0f);
+           // }
+            }
+
+            if (player2)
+            {
+          //  if (player2Lives != 0)
+        //    {
+                Instantiate(player2Object);
+                player2Object.transform.position = new Vector3(0f, 2.0f, 0.0f);
+         //   }
+            }
+
+            if (player3)
+            {
+         //       if (player3Lives != 0)
+         //       {
+                    Instantiate(player3Object);
+                    player3Object.transform.position = new Vector3(5f, 2.0f, 0.0f);
+         //       }
+            }
+
+            if (player4)
+            {
+         //           if (player4Lives != 0)
+          //          {
+                        Instantiate(player4Object);
+                        player4Object.transform.position = new Vector3(10f, 2.0f, 0.0f);
+         //           }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rig;
+
+    public float speed = 1.0f;
+    public float max_force = 20f;
+    public Vector2 moveDirection = Vector2.zero;
+    public float jump_height = 1.0f;
+    public float gravity = 50.0f;
+    public float jump_force = 50.0f;
+    public Vector2 impulse_force = Vector2.zero;
+	public AudioSource jump;
+
+    private bool knockedOutRef;
+
+    private bool grounded = false;
+	private string axis_string = "";
+	private string jump_string = "";
+	private string player_name = "";
+    /*.................................................
+      IMPULSE BASED X,Y MOVEMENT
+      (works much better using gamepad)
+      .................................................
+    */
+
+
+    void Start()
+    {
+        //Initialisations
+        rig = GetComponent<Rigidbody2D>();
+        rig.mass = 5.0f;
+        rig.drag = 5.0f;
+        rig.freezeRotation = true;
+        rig.gravityScale = 0.0f;
+		if (gameObject.tag == "Player") {
+			axis_string = "Horizontal";
+			jump_string = "Jump";
+			player_name = "Player";
+		}
+		if (gameObject.tag == "Player2") {
+			axis_string = "Horizontal2";
+			jump_string = "P2 Jump";
+			player_name = "Player2";
+		}
+		if (gameObject.tag == "Player3") {
+			axis_string = "Horizontal3";
+			jump_string = "P3 Jump";
+			player_name = "Player3";
+		}
+		if (gameObject.tag == "Player4") {
+			axis_string = "Horizontal4";
+			jump_string = "P4 Jump";
+			player_name = "Player4";
+		}
+
+    }
+
+
+    //Update is called once per frame
+    void FixedUpdate()
+    {
+        if (gameObject.tag == player_name && knockedOutRef == false)
+        {
+            moveDirection.x = (Input.GetAxis(axis_string));
+            if (moveDirection.x < 0.1 && moveDirection.x > -0.1)
+            {
+                moveDirection.x = 0;
+            }
+            impulse_force = Vector2.ClampMagnitude((speed * moveDirection * rig.mass), max_force);
+
+            if (grounded)
+            {
+				if(Input.GetButton(jump_string)  )
+                {
+                    rig.AddForce(new Vector2(0, jump_force * rig.mass));
+					jump.Play ();
+                }
+                
+            }
+        }
+        if (!grounded)
+        {
+            moveDirection.y = 0;
+        }
+
+        rig.AddForce(new Vector2(0, -gravity * rig.mass));
+        rig.AddForce(impulse_force, ForceMode2D.Impulse);
+        moveDirection = Vector2.zero;
+        impulse_force = Vector2.zero;
+    }
+
+
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+		if (col.gameObject.tag == "Ground" || col.gameObject.tag == "Player")
+        {
+            grounded = true;
+        }
+
+    }
+
+
+    void OnCollisionExit2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Ground")
+        {
+            grounded = false;
+        }
+    }
+
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.tag == "Ground")
+        {
+            transform.parent = col.gameObject.transform;
+        }
+    }
+
+    //Not working, but could be?
+    //public bool isGrounded()
+    //{
+    //    return Physics.Raycast(rig.transform.position, -Vector2.up, dist_to_ground);
+    //}
+
+}
