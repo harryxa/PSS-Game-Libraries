b1afa92d7f84c752b7b16f66e13832fbdb49a663
Mythalore <samfrichards@gmail.com>
2017-10-18 10:45:50 +0100

    SCREEN SHAKE!

---
+++ Assets/Scripts/CameraShake.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class ShakeEffect
+{
+    private float stength;
+    private float length;
+    private float lifeTime;
+    private float targetStength = -1f;
+
+    public ShakeEffect(float lifeTime, float stength)
+    {
+        this.lifeTime = lifeTime;
+        this.length = this.lifeTime;
+        this.stength = stength;
+    }
+
+    public void TransitionStrength(float targetStrength)
+    {
+        this.targetStength = targetStrength;
+        if (this.targetStength < 0) this.targetStength = 0;
+    }
+
+    public float GetLength()
+    {
+        return length;
+    }
+    public void UpdateLength()
+    {
+        length -= Time.deltaTime;
+    }
+
+    public float GetStrength()
+    {
+        if (targetStength >= 0)
+        {
+            return Mathf.Lerp(targetStength, stength, length/lifeTime);
+        }
+        else
+        {
+            return stength;
+        }
+    }
+}
+
+public class CameraShake : MonoBehaviour {
+    
+	public Camera cameraToShake;
+    private List<ShakeEffect> shakeEffects;
+    
+    void Start () {
+        shakeEffects = new List<ShakeEffect>();
+    }
+	
+	void Update ()
+    {
+        for (int i = 0; i < shakeEffects.Count; i++)
+        {
+            if (shakeEffects[i].GetLength() > 0)
+            {
+                shakeEffects[i].UpdateLength();
+            }
+            else
+            {
+                shakeEffects.RemoveAt(i);
+            }
+        }
+        if (shakeEffects.Count > 0)
+        {
+            float strengthMultiplier = 0;
+            for (int i = 0; i < shakeEffects.Count; i++)
+            {
+                if (shakeEffects[i].GetStrength() > strengthMultiplier)
+                {
+                    strengthMultiplier = shakeEffects[i].GetStrength();
+                }
+            }
+            float shakeRange = 0.2f * strengthMultiplier;
+            cameraToShake.transform.position = new Vector3(cameraToShake.transform.position.x + Random.Range(-shakeRange, shakeRange), cameraToShake.transform.position.y + Random.Range(-shakeRange, shakeRange), -10);
+        }
+        else
+        {
+            //cameraToShake.transform.position = new Vector3(0, 0, -10);
+        }
+    }
+
+    public void ShakeCamera(float length, float stength)
+    {
+        ShakeEffect newEffect = new ShakeEffect(length, stength);
+        shakeEffects.Add(newEffect);
+    }
+
+    public void ShakeCamera(float length, float stength, float fadeToStrength)
+    {
+        ShakeEffect newEffect = new ShakeEffect(length, stength);
+        newEffect.TransitionStrength(fadeToStrength);
+        shakeEffects.Add(newEffect);
+    }
+
+    public void StopCamera()
+    {
+        shakeEffects.Clear();
+    }
+}
